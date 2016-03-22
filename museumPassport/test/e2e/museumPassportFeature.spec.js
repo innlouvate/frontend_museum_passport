bd5cabe4885cb09d4fa706ee9262e3adae7f2fc7
innlouvate <l.j.franklin85@googlemail.com>
2016-03-22 09:48:21 +0000

    dev branch

--- museumPassport/test/e2e/museumPassportFeature.spec.js
+++ museumPassport/test/e2e/museumPassportFeature.spec.js
@@ -5,11 +5,6 @@ describe('hello world', function(){
     browser.get('http://localhost:8100');
   });
 
-  it('should display hello world', function(){
-    var text = element(by.id('text'));
-    expect(text.getText()).toContain('Hello world');
-  });
-
   it('should display text', function(){
     var title = element(by.css('.title'));
     expect(title.getText()).toContain('Museum Passport');
