892aeaefa71cafcda577162e3bc43ebda2544d29
Vicky Gray <vickygray26@hotmail.com>
2016-03-28 23:04:50 +0100

    further refactor of feature test

--- museumPassport/test/e2e/museumPassportFeature.spec.js
+++ museumPassport/test/e2e/museumPassportFeature.spec.js
@@ -7,9 +7,20 @@ describe('museumPassport', function(){
     element(by.buttonText('Login')).click();
   });
 
-  it('should display text', function(){
+  it('should display title', function(){
     var title = element(by.css('.title'));
     expect(title.getText()).toContain('Museum Passport');
+  });
+
+  it('should have tabs', function() {
+    var tab = element.all(by.id('question-icon'));
+    expect(tab.count()).toEqual(1);
+  });
+
+  it('should display a sign-out icon', function() {
+    expect(browser.getCurrentUrl()).toContain('home');
+    expect(element.all(by.repeater('museum in museums')).count()).toEqual(4);
+    expect(element(by.buttonText('Museum 1')).isPresent()).toBeTruthy();
   });
 
   it('should direct to the homepage on login', function(){
@@ -25,29 +36,23 @@ describe('museumPassport', function(){
     expect(element(by.buttonText('Exhibit 1')).isPresent()).toBeTruthy();
   });
 
-  it('should direct to the questions page when clicking on exhibits button', function(){
-    element(by.buttonText('Museum 1')).click();
-    element(by.buttonText('Exhibit 1')).click();
-    expect(browser.getCurrentUrl()).toContain('questions');
-  });
+  describe('questions', function (){
 
-  it('should display 4 questions', function(){
-    element(by.buttonText('Museum 1')).click();
-    element(by.buttonText('Exhibit 1')).click();
-    var questions = element.all(by.css('.question'));
-    expect(questions.count()).toEqual(4);
-    expect(questions.first().getText()).toEqual('question one');
-  });
+    beforeEach(function() {
+      element(by.buttonText('Museum 1')).click();
+      element(by.buttonText('Exhibit 1')).click();
+    });
 
-  it('should have tabs', function() {
-    var tab = element.all(by.id('question-icon'));
-    expect(tab.count()).toEqual(1);
-  });
+    it('should direct to the questions page when clicking on exhibits button', function(){
+      expect(browser.getCurrentUrl()).toContain('questions');
+    });
 
-  it('should display a sign-out icon', function() {
-    expect(browser.getCurrentUrl()).toContain('home');
-    expect(element.all(by.repeater('museum in museums')).count()).toEqual(4);
-    expect(element(by.buttonText('Museum 1')).isPresent()).toBeTruthy();
+    it('should display 4 questions', function(){
+      var questions = element.all(by.css('.question'));
+      expect(questions.count()).toEqual(4);
+      expect(questions.first().getText()).toEqual('question one');
+    });
+
   });
 
 });
