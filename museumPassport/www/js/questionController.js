da2cdb96d0852bf68730d6281a94b6033596682a
innlouvate <l.j.franklin85@googlemail.com>
2016-03-29 19:16:20 +0100

    clean

--- museumPassport/www/js/questionController.js
+++ museumPassport/www/js/questionController.js
@@ -1,7 +1,7 @@
 (function() {
 angular
   .module('museumPassport.questions', ['ionic'])
-  .controller('QuestionController', function($scope, $http, $cordovaFile, $ionicPlatform, $ionicActionSheet, ImageService, FileService){
+  .controller('QuestionController', function($scope, $http){
 
   $http.get('https://museum-passport-backend.herokuapp.com/museums/0/exhibits/0/questions').success(function(data){
     $scope.questions = data;
@@ -43,37 +43,6 @@ angular
  //    //  template: "Where to next?"
  //   });
  // };
-
- $ionicPlatform.ready(function() {
-   $scope.images = FileService.images();
-   $scope.$evalAsync();
- });
-
- $scope.urlForImage = function(imageName) {
-   var trueOrigin = cordova.file.dataDirectory + imageName;
-   return trueOrigin;
- }
-
- $scope.addMedia = function() {
-   $scope.hideSheet = $ionicActionSheet.show({
-     buttons: [
-       { text: 'Take photo' },
-       { text: 'Photo from library' }
-     ],
-     titleText: 'Add images',
-     cancelText: 'Cancel',
-     buttonClicked: function(index) {
-       $scope.addImage(index);
-     }
-   });
- }
-
- $scope.addImage = function(type) {
-   $scope.hideSheet();
-   ImageService.handleMediaDialog(type).then(function() {
-     $scope.$evalAsync();
-   });
- }
 
 });
 })();
