9d0e7b9aef1a407eeca016f65b76e8c122ef0ce0
innlouvate <l.j.franklin85@googlemail.com>
2016-03-29 19:13:23 +0100

    clean up

--- museumPassport/www/js/controllers/questionController.js
+++ museumPassport/www/js/controllers/questionController.js
@@ -6,8 +6,6 @@ angular
 
   $http.get('http://localhost:3000/exhibits/' + localStorage['exhibitId'] + '/questions').success(function(data){
     $scope.questions = data;
-    $scope.status();
-    console.log($scope.status())
   });
 
   $scope.status = function() {
@@ -38,22 +36,6 @@ angular
     var data = JSON.stringify({"entry": answer, "user_id": window.localStorage['userId']});
     return data;
   };
-
-  // $scope.updateAnswer = function(questionID, answerID, answer) {
-  //       var data = $scope.formatUpdate(answerID, answer);
-  //
-  //       $http({
-  //         method: 'PUT',
-  //         url:    'http://localhost:3000/museums/1/exhibits/1/questions/'+questionID+'/answers/'+answerID+'.json',
-  //         data:   data,
-  //         headers: { 'Content-Type': 'application/json'}
-  //       })
-  //         .success(function ( data, status, header, JSON ) {
-  //         })
-  //         .error(function ( data, status, header, JSON ) {
-  //         });
-  //       console.log(data);
-  //     };
 
   $scope.formatUpdate = function(answer_id, answer) {
     var data = JSON.stringify({"answer_id": answer_id, "entry": answer, "user_id": window.localStorage['userId']});
