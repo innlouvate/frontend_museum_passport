988ccdec847cb82f4741dd82914a9437cc22c9a1
innlouvate <l.j.franklin85@googlemail.com>
2016-03-31 20:54:25 +0100

    not yet working - some tweaks to code to try to reload questions page

---
+++ museumPassport/www/js/controllers/FullscreenImageController.js
@@ -0,0 +1,43 @@
+angular
+  .module('museumPassport.fullscreenImage', [])
+  .factory('FullscreenImageCtrl', function ($scope, $ionicModal) {
+
+    $ionicModal.fromTemplateUrl('image-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+
+    $scope.openModal = function() {
+      $scope.modal.show();
+    };
+
+    $scope.closeModal = function() {
+      $scope.modal.hide();
+    };
+
+    //Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hide', function() {
+      // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+      // Execute action
+    });
+    $scope.$on('modal.shown', function() {
+      console.log('Modal is shown!');
+    });
+
+    $scope.imageSrc = '';
+
+    $scope.showImage = function(url) {
+      $scope.imageSrc  = url;
+      $scope.openModal();
+    }
+
+  });
--- museumPassport/www/js/controllers/questionController.js
+++ museumPassport/www/js/controllers/questionController.js
@@ -1,12 +1,18 @@
 (function() {
 angular
   .module('museumPassport.questions', [])
-  .controller('QuestionController', function($scope, $http, $cordovaFile, $cordovaDevice, $ionicActionSheet, $ionicPlatform, CreateAnswer, EditAnswer, FileService, Photo, Response){
+  .controller('QuestionController', function($scope, $http, $cordovaFile, $cordovaDevice, $ionicActionSheet, $ionicPlatform, $ionicModal, $ionicPopup, CreateAnswer, EditAnswer, FileService, Photo, Response){
 
 
   $http.get('http://museum-passport-backend.herokuapp.com/exhibits/' + localStorage['exhibitId'] + '/questions').success(function(data){
     $scope.questions = data;
   });
+
+  $scope.getData = function() {
+    $http.get('http://museum-passport-backend.herokuapp.com/exhibits/' + localStorage['exhibitId'] + '/questions').success(function(data){
+      $scope.questions = data;
+    });
+  }
 
   $scope.collectResponses = function() {
     $scope.questions.forEach(function(item) {
@@ -22,17 +28,41 @@ angular
 
   $scope.addImage = function(question) {
     Photo.takePhoto(question)
-    .then(function() {
-      $scope.$evalAsync();
+    .then(function(result) {
+      $scope.getData();
+      // $scope.$evalAsync();
+    //   // $state.go($state.current, {}, {reload: true});
+    //   // $window.location.reload(true)
     });
   }
 
- //  $scope.savedAlert = function() {
- //   var alertPopup = $ionicPopup.alert({
- //     title: "Your responses have been saved",
- //    //  template: "Where to next?"
- //   });
- // };
+  $scope.savedAlert = function() {
+   var alertPopup = $ionicPopup.alert({
+     title: "Your responses have been saved",
+     template: "Where to next?"
+   });
+ };
+
+ $scope.showImage = function(question) {
+		$scope.image = question.question.image;
+		$scope.showModal('templates/imageModal.html');
+	}
+
+	$scope.showModal = function(templateUrl) {
+		$ionicModal.fromTemplateUrl(templateUrl, {
+			scope: $scope,
+			animation: 'slide-in-up'
+		}).then(function(modal) {
+			$scope.modal = modal;
+			$scope.modal.show();
+		});
+	}
+
+	// Close the modal
+	$scope.closeModal = function() {
+		$scope.modal.hide();
+		$scope.modal.remove()
+	};
 
 });
 })();
